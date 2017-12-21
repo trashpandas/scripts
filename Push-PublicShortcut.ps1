e544195152d9fd805db83b62d533ae8ef93bc8aa
Patrick Huber <phgithub@icloud.com>
2017-12-21 10:48:20 -0500

    Changed select to Select-Object and foreach to ForEach-Object

--- Push-PublicShortcut.ps1
+++ Push-PublicShortcut.ps1
@@ -4,10 +4,10 @@ $SearchBase = Read-Host "OU distinguishedName"
 $SourceFile = Read-Host "Shortcut File Location"
 
 # Pull computer names
-$Computers = Get-ADComputer -Filter * -SearchBase $SearchBase | Select -Expand Name
+$Computers = Get-ADComputer -Filter * -SearchBase $SearchBase | Select-Object -Expand Name
 
 # Set the file destination as the public desktop
 $Destination = "Users\Public\Desktop"
 
 # Copy shorcut to all computers in $Computers
-$Computers | foreach {Copy-Item $SourceFile -Destination \\$_\c$\$Destination}
+$Computers | ForEach-Object {Copy-Item $SourceFile -Destination \\$_\c$\$Destination}
