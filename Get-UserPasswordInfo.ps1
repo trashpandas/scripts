d46333c0a2cebe9168cc90b2d7336b54be9b889f
Patrick Huber <phgithub@icloud.com>
2017-12-21 09:52:10 -0500

    Formatting and removed unused variable

--- Get-UserPasswordInfo.ps1
+++ Get-UserPasswordInfo.ps1
@@ -1,9 +1,5 @@
-﻿# Creates CSV of all AD user's PasswordLastSet, LastLogonDate, PasswordNeverExpires
-
-$PassInfo = Get-ADUser -Filter {Enabled -eq $true} -Properties PasswordLastSet, LastLogonDate, PasswordNeverExpires |
-
+﻿# Creates CSV of all AD users PasswordLastSet, LastLogonDate, PasswordNeverExpires
+Get-ADUser -Filter {Enabled -eq $true} -Properties PasswordLastSet, LastLogonDate, PasswordNeverExpires |
 Sort-Object Name |
-
 Select-Object Name, PasswordLastSet, LastLogonDate, PasswordNeverExpires |
-
 Export-CSV -Path C:\Temp\user-password-info.csv -NoTypeInformation
