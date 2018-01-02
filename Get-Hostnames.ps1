147f4fc5e79516c55c2f6d5b6f58fec83e6d60e7
Patrick Huber <phproxy@icloud.com>
2018-01-02 09:23:22 -0500

    Updated script syntax and gitignore

--- Get-Hostnames.ps1
+++ Get-Hostnames.ps1
@@ -9,7 +9,7 @@ $SearchBase = Read-Host "OU distinguishedName"
 $OutputFile = Read-Host "Output File Location"
 
 # Pull computer names
-$Computers = Get-ADComputer -Filter * -SearchBase "$SearchBase" | Select Name
+$Computers = Get-ADComputer -Filter * -SearchBase "$SearchBase" | Select-Object Name
 
 # Export CSV to path set in $OutputFile parameter
 $Computers | Export-CSV -NoTypeInformation -Path "$OutputFile"
